d8b308c3dc6c610ffd1d98e7b0c8365036afcbe3
Egor <nullatnodes@icloud.com>
2022-08-19 01:19:02 +0700

    Rewrite projects page to satisfy ProjectType

--- components/project/ProjectCard.tsx
+++ components/project/ProjectCard.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import Link from "next/link";
 import ProjectStatus from "./ProjectStatus";
+import ProjectType from "../../types/project.interface";
 
-interface Props {
-    id: number;
-    title: string;
-    logo: string;
-    description: string;
-    onGoing: boolean;
-}
+type Props = Pick<ProjectType, "id" | "logoUrl" | "title" | "description" | "onGoing">
 
-const ProjectCard = ({id, title, logo, description, onGoing}: Props) => {
+const ProjectCard = ({id, title, onGoing, logoUrl, description}: Props) => {
     return (
         <Link
             href={`/projects/${id}`}
         >
             <div  className={`relative w-3/4 sm:w-full flex flex-col items-center justify-center bg-sky-700 hover:bg-sky-600 rounded-lg p-4 flex-nowrap min-h-full cursor-pointer`}>
-                <img className={'w-20 mx-auto rounded-full'} src={`${process.env.NEXT_PUBLIC_API}/${logo}`}/>
+                <img className={'w-20 mx-auto rounded-full'} src={`${process.env.NEXT_PUBLIC_API}/${logoUrl}`}/>
                 <span className={'py-2 inline-block w-full text-center'}>{title}</span>
                 <p className={'inline-block w-full text-start'}>{description}</p>
                 <ProjectStatus className={'absolute top-1 right-1'} onGoing={onGoing} />
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,28 +2,21 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import ProjectCard from "../components/project/ProjectCard";
 import {GetStaticProps, NextPage} from "next";
-
-interface Project {
-    id: number;
-    title: string;
-    description: string;
-    logo: string;
-    onGoing: boolean;
-}
+import ProjectType from "../types/project.interface";
 
 interface Props {
-    projects: Project[];
+    projects: ProjectType[];
 }
 
 export async function getStaticProps() {
     try {
         const response = await axios.get('http://localhost:3000/projects');
-        const mappedProjects = response.data.map((project: Project) => {
+        const mappedProjects = response.data.map((project: ProjectType) => {
             return {
                 id: project.id,
                 title: project.title,
                 description: project.description,
-                logo: project.logo,
+                logoUrl: project.logoUrl,
                 onGoing: project.onGoing,
             }
         });
@@ -43,13 +36,13 @@ const Projects: NextPage<Props> = ({projects}) => {
         <div className="min-h-screen">
             <span className={'text-2xl inline-block w-full text-center p-2'}>My projects</span>
             <div  className={'p-5 flex flex-col sm:grid sm:grid-cols-3 gap-4 items-center justify-center'}>
-                {projects?.map((project: Project) => (
+                {projects?.map((project) => (
                     <ProjectCard
                         key={project.id}
                         id={project.id}
                         title={project.title}
                         description={project.description}
-                        logo={project.logo}
+                        logoUrl={project.logoUrl}
                         onGoing={project.onGoing}
                     />
                 ))
