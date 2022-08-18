72290b83b21dc4e562c834a61e4a5f1a97d6888d
Egor <nullatnodes@icloud.com>
2022-08-19 01:43:17 +0700

    Rewrite project page to satisfy ProjectType

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -7,25 +7,10 @@ import ReactMarkdown from 'react-markdown'
 import axios from "axios";
 import {GetStaticProps, GetStaticPaths, NextPage} from "next";
 import { ParsedUrlQuery } from 'querystring';
-
-interface Project {
-    id: string;
-    title: string;
-    description: string;
-    stage: string;
-    logo: string;
-    website: string;
-    discord: string;
-    github: string;
-    telegram: string;
-    summary: string;
-    startDate: string;
-    involvement: string;
-    guide: string;
-}
+import ProjectType from "../../types/project.interface";
 
 interface Props {
-    project: Project;
+    project: ProjectType;
 }
 
 interface Params extends ParsedUrlQuery {
@@ -34,9 +19,9 @@ interface Params extends ParsedUrlQuery {
 
 export const getStaticPaths: GetStaticPaths<Params> = async () => {
     const response = await axios.get('http://localhost:3000/projects');
-    const paths = response.data.map((project: Project) => {
+    const paths = response.data.map((project: ProjectType) => {
         return {
-            params: {id: project.id.toString()}
+            params: {id: project.id?.toString()}
         }
     })
     return {
@@ -49,7 +34,7 @@ export const getStaticProps: GetStaticProps<Props, Params> = async(context) => {
     try {
         const {id} = context.params!
         const response = await axios.get(`http://localhost:3000/projects/getProject/${id}`);
-        const project: Project = response.data;
+        const project: ProjectType = response.data;
         return {
             props: {
                 project
@@ -62,52 +47,45 @@ export const getStaticProps: GetStaticProps<Props, Params> = async(context) => {
 
 const Project: NextPage<Props> = ({project}) => {
 
-   /* if (!project) {
-        return (
-            <div className={'bg-gray-800 min-h-screen flex items-center justify-center'}>
-                <Loader className={'w-10 h-10'}></Loader>
-            </div>
-        )
-    }*/
-
     return (
         <>
             <div className={'min-h-screen p-5 w-full flex flex-col gap-4 items-center mx-auto'}>
                 <div
-                    className={'w-1/2 p-2 flex flex-col justify-evenly sm:flex-row sm:flex-wrap items-center'}>
+                    className={'w-1/2 p-2 flex flex-col justify-evenly gap-1 sm:gap-0 sm:flex-row items-center'}>
                     <img
-                        className={'w-20'}
-                        src={`${process.env.NEXT_PUBLIC_API}/${project.logo}`}
+                        className={'w-20 inline-block rounded-full p-1'}
+                        src={`${process.env.NEXT_PUBLIC_API}/${project.logoUrl}`}
+                        alt={'project logo'}
                     />
-                    <div className={'text-center'}>
+                    <div className={'text-center p-1'}>
                         <span className={'text-4xl sm:text-5xl font-medium'}>{project.title}</span>
                     </div>
-                    <div className={'flex flex-col items-center gap-2 p-2 font-medium text-base text-black sm:text-lg'}>
+                    <div className={'flex flex-col items-center gap-2 p-1 font-medium text-lg text-black sm:text-base'}>
                         <div className={'bg-white rounded-md p-2 w-full'}>
                             <p className={'text-center'}>{project.stage}</p>
                         </div>
-                        <div className={'bg-white rounded-md p-2 w-full flex flex-row gap-2'}>
-                            <a href={project.website}><img className={'fill-sky-700 w-5'} src={'/website.svg'}/></a>
-                            <a href={project.github}><img className={'fill-sky-700 w-5'} src={'/github.svg'}/></a>
-                            <a href={project.discord}><img className={'fill-sky-700 w-5'} src={'/discord.svg'}/></a>
-                            <a href={project.telegram}><img className={'fill-sky-700 w-5'} src={'/telegram.svg'}/></a>
+                        <div className={'bg-white rounded-md p-2 min-w-max flex flex-row gap-2'}>
+                            <a href={project.website}><img className={'w-5'} src={'/website.svg'} alt={'website logo'}/></a>
+                            <a href={project.github}><img className={'w-5'} src={'/github.svg'} alt={'github logo'}/></a>
+                            <a href={project.discord}><img className={'w-5'} src={'/discord.svg'} alt={'discord logo'}/></a>
+                            <a href={project.telegram}><img className={'w-5'} src={'/telegram.svg'} alt={'tg logo'}/></a>
                         </div>
                     </div>
                 </div>
                 <div className={'mt-2 grid sm:grid-cols-2 gap-5 w-3/4'}>
-                    <div className={'border-sky-800 border-2  p-4 flex flex-col'}>
-                        <p className={'text-center font-medium text-lg bg-sky-900 mb-2 rounded-md'}>Summary</p>
+                    <div className={'border-sky-700 border-2 rounded-md p-4 flex flex-col'}>
+                        <p className={'text-center font-medium text-lg bg-sky-700 mb-2 rounded-md'}>Summary</p>
                         <ReactMarkdown className={'prose prose-invert'}>{project.summary}</ReactMarkdown>
                     </div>
-                    <div className={'border-sky-800 border-2 p-4 flex flex-col'}>
-                        <p className={'text-center font-medium text-lg bg-sky-900 mb-2 rounded-md'}>Contribution</p>
-                        <ReactMarkdown className={'prose prose-md prose-invert'}>{project.involvement}</ReactMarkdown>
+                    <div className={'border-sky-700 border-2 rounded-md p-4 flex flex-col'}>
+                        <p className={'text-center font-medium text-lg bg-sky-700 mb-2 rounded-md'}>Contribution</p>
+                        <ReactMarkdown className={'prose prose-md prose-invert m-0'}>{project.involvement}</ReactMarkdown>
                     </div>
                 </div>
                 <div className={'mt-2 flex flex-col w-3/4'}>
-                    <div className={'border-sky-800 border-2 p-4 flex flex-col'}>
-                        <h1 className={'text-center font-medium text-lg bg-sky-900 mb-2 rounded-md'}>Guide</h1>
-                        <p>{project.guide}</p>
+                    <div className={'border-sky-700 border-2 rounded-md p-4 flex flex-col'}>
+                        <h1 className={'text-center font-medium text-lg bg-sky-700 mb-2 rounded-md'}>Guide</h1>
+                        <ReactMarkdown className={'prose prose-invert'}>{project.guide}</ReactMarkdown>
                     </div>
                 </div>
             </div>
